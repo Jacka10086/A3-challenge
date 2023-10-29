25c94d9b81ad5dbe0ec13e3452a0426657be8ac6
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-29 04:49:03 +0000

    Update Classroom.java

--- Classroom.java
+++ Classroom.java
@@ -3,9 +3,12 @@ import java.util.Random;
 public class Classroom {
 
     public static void main(String[] args) {
-
-        String[] names = { "Adrian", "Bea", "Colin", "Deborah", "Eric", "Freida", "Gary", "Harriet", "Ian", "Janet" };
-        ArrayBag students = new ArrayBag(capacity: 100);
+        String[] names = {
+            "Adrian", "Bea", "Colin", "Deborah", "Eric",
+            "Freida", "Gary", "Harriet", "Ian", "Janet"
+        };
+        
+        ArrayBag students = new ArrayBag(100);
         Random random = new Random();
 
         for (int i = 0; i < 100; i++) {
@@ -16,13 +19,29 @@ public class Classroom {
 
         int mostFrequent = 0;
         for (String name : names) {
-            if (students.getFrequencyOf(name) > mostFrequent)
+            if (students.getFrequencyOf(name) > mostFrequent) {
                 mostFrequent = students.getFrequencyOf(name);
+            }
         }
 
         System.out.printf("\nThe following names appear %d times and have been removed\n", mostFrequent);
         for (String name : names) {
-            // 这里似乎遗漏了一部分逻辑
+            if (students.getFrequencyOf(name) == mostFrequent) {
+                System.out.println(name);
+                while (students.contains(name)) {
+                    students.remove(name);
+                }
+            }
+        }
+
+        System.out.printf("\n%d students remaining\n\n", students.getCurrentSize());
+
+        System.out.printf("%-10s %-5s\n", "Name", "Count");
+        System.out.println("----------");
+        for (String name : names) {
+            if (students.getFrequencyOf(name) > 0) {
+                System.out.printf("%-10s %-5d\n", name, students.getFrequencyOf(name));
+            }
         }
     }
 }
