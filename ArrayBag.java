f9850173438438c53517268d2476450db482185e
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-30 01:15:17 +0000

    Update ArrayBag.java

--- ArrayBag.java
+++ ArrayBag.java
@@ -1,160 +1,156 @@
-package A3CH;
 
-import java.util.HashMap;
+package Assignments.Com410.A4.Anytown; // 定义包名为Anytown
 
-public class ArrayBag implements BagInterface<String> {
+// 定义一个名为ArrayBag的公共类，它实现了BagInterface接口
+public class ArrayBag implements BagInterface {
 
-    private String[] bag;
-    private int numberOfEntries;
-    private static final int DEFAULT_CAPACITY = 100;
+    private Building[] bag; // 定义一个Building类型的数组，用于存储元素
+    private int numberOfEntries; // 定义一个整数，表示数组中的实际元素数量
+    private static final int DEFAULT_CAPACITY = 25; // 定义一个静态常量，表示数组的默认容量
 
+    // 默认构造函数，初始化数组的容量为默认容量
     public ArrayBag() {
-        this(DEFAULT_CAPACITY);
+        this(ArrayBag.DEFAULT_CAPACITY);
     }
 
+    // 带参数的构造函数，根据给定的容量初始化数组
     public ArrayBag(int capacity) {
-        this.bag = new String[capacity];
+        this.bag = new Building[capacity];
         this.numberOfEntries = 0;
     }
 
+    // 返回数组中的实际元素数量
     public int getCurrentSize() {
         return this.numberOfEntries;
     }
 
+    // 判断数组是否为空
     public boolean isEmpty() {
         return this.numberOfEntries == 0;
     }
 
-    public boolean isFull() {
+    // 判断数组是否已满
+    public boolean isArrayFull() {
         return this.numberOfEntries == this.bag.length;
     }
 
-    public boolean add(String newEntry) {
-        if (this.isFull()) {
+    // 向数组中添加一个新元素，如果数组已满则返回false，否则返回true
+    public boolean addNewEntry(Building newEntry) {
+        if (isArrayFull()) {
             return false;
         } else {
-            this.bag[this.numberOfEntries] = newEntry;
-            this.numberOfEntries++;
+            this.bag[this.numberOfEntries++] = newEntry;
             return true;
         }
     }
 
-    public String remove() {
-        if (this.isEmpty()) {
-            return null;
-        } else {
-            String removedEntry = this.bag[this.numberOfEntries - 1];
-            this.bag[this.numberOfEntries - 1] = null;
-            this.numberOfEntries--;
-            return removedEntry;
-        }
+    // 私有方法，从指定索引处删除元素，并返回被删除的元素
+private Building removeElmentAt(int index) {
+    // 初始化返回结果为null
+    Building result = null;
+
+    // 检查数组是否非空，并且索引是否在有效范围内
+    if (!isEmpty() && (index >= 0 && index < this.numberOfEntries)) {
+        // 获取要移除的Building对象
+        result = this.bag[index];
+
+        // 将数组的最后一个元素移到要移除的位置，这样可以保持数组的连续性
+        this.bag[index] = this.bag[numberOfEntries - 1];
+
+        // 将最后一个元素设置为null，表示已经移除
+        this.bag[numberOfEntries - 1] = null;
+
+        // 更新数组中的元素数量
+        this.numberOfEntries--;
     }
 
-    public boolean remove(String anEntry) {
-        for (int i = 0; i < this.numberOfEntries; i++) {
-            if (this.bag[i].equals(anEntry)) {
-                this.bag[i] = this.bag[this.numberOfEntries - 1];
-                this.bag[this.numberOfEntries - 1] = null;
-                this.numberOfEntries--;
-                return true;
-            }
-        }
-        return false;
+    // 返回被移除的Building对象
+    return result;
+}
+
+    // 删除并返回数组中的最后一个元素
+    public Building remove() {
+        return removeElmentAt(this.numberOfEntries - 1);
     }
 
-    public void clear() {
-        while (!this.isEmpty()) {
-            this.remove();
-        }
-    }
 
-    public int getFrequencyOf(String anEntry) {
-        int frequency = 0;
-        for (int i = 0; i < this.numberOfEntries; i++) {
-            if (this.bag[i].equals(anEntry)) {
-                frequency++;
-            }
-        }
-        return frequency;
-    }
-
-    public boolean contains(String anEntry) {
-        for (int i = 0; i < this.numberOfEntries; i++) {
-            if (this.bag[i].equals(anEntry)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public String[] toArray() {
-        String[] result = new String[this.numberOfEntries];
-        for (int i = 0; i < this.numberOfEntries; i++) {
-            result[i] = this.bag[i];
-        }
-        return result;
-    }
-
-    public void displayBag() {
-        System.out.println("  100 students generated: ");
-    }
-
-    public String findMostFrequentName() {
-        HashMap<String, Integer> nameCounts = new HashMap<String, Integer>();
-        for (int i = 0; i < this.numberOfEntries; i++) {
-            String name = this.bag[i];
-            if (nameCounts.containsKey(name)) {
-                nameCounts.put(name, nameCounts.get(name) + 1);
+    
+    // 删除指定的元素，如果删除成功则返回true，否则返回false
+    public boolean remove(Building anEntry) {
+        // 初始化一个标志位，用于标记是否找到了要删除的元素
+        boolean found = false;
+        
+        // 初始化一个索引变量，从0开始
+        int index = 0;
+        
+        // 当还没有找到要删除的元素并且索引小于数组的长度时，继续循环
+        while(!found && (index < this.numberOfEntries)) {
+            // 如果当前索引位置的元素与要删除的元素相等
+            if (this.bag[index].equals(anEntry)) {
+                // 设置标志位为true，表示找到了要删除的元素
+                found = true;
             } else {
-                nameCounts.put(name, 1);
+                // 如果没有找到，索引加1，继续查找
+                index++;
             }
         }
-        int maxCount = 0;
-        for (int count : nameCounts.values()) {
-            if (count > maxCount) {
-                maxCount = count;
-            }
+        
+        // 如果找到了要删除的元素
+        if (found) {
+            // 调用removeElmentAt方法，根据索引删除元素
+            removeElmentAt(index);
         }
-        String mostFrequentName = "";
-        for (String name : nameCounts.keySet()) {
-            int count = nameCounts.get(name);
-            if (count == maxCount) {
-                mostFrequentName += name + ", ";
-            }
-        }
-
-        mostFrequentName = mostFrequentName.substring(0, mostFrequentName.length() - 2);
-        System.out.println("The following names appear " + maxCount + " times and have been removed: \n\n    " + mostFrequentName);
-        return mostFrequentName;
+        
+        // 返回是否成功删除的标志位
+        return found;
     }
-    public void displayRemainingNames(int remainingStudents) {
-    HashMap<String, Integer> nameCounts = new HashMap<String, Integer>();
+
+
+
+    
+    // 清空数组中的所有元素
+    public void clear() {
+        while (!isEmpty()) 
+            remove();
+    }
+
+   // 返回指定元素在数组中的出现次数
+public int getFrequencyOf(Building anEntry) {
+    int count = 0;  // 初始化计数器为0
+    // 遍历数组
     for (int i = 0; i < this.numberOfEntries; i++) {
-        String name = this.bag[i];
-        if (nameCounts.containsKey(name)) {
-            nameCounts.put(name, nameCounts.get(name) + 1);
-        } else {
-            nameCounts.put(name, 1);
+        // 如果当前元素等于目标元素
+        if (this.bag[i].equals(anEntry)) {
+            count++;  // 计数器加1
         }
     }
-    String mostFrequentName = "";
-    int highestFrequency = 0;
-    for (String name : nameCounts.keySet()) {
-        int count = nameCounts.get(name);
-        if (count > highestFrequency) {
-            mostFrequentName = name;
-            highestFrequency = count;
-        }
-    }
-    nameCounts.remove(mostFrequentName);
-    System.out.println("Name       Count");
-    System.out.println("-----------------");
-    for (String name : nameCounts.keySet()) {
-        int count = nameCounts.get(name);
-        if (count > 0) {
-            System.out.printf("%1$-10s %2$-5d\n\n", name, count);
-        }
-    }
+    return count;  // 返回目标元素在数组中的出现次数
+}
 
+    // 判断数组中是否包含指定的元素
+public boolean contains(Building anEntry) {
+    boolean found = false;  // 初始化找到标志为false
+    int index = 0;  // 初始化索引为0
+    // 当未找到元素且索引小于数组长度时，继续循环
+    while(!found && (index < this.numberOfEntries)) {
+        // 如果当前元素等于目标元素
+        if (this.bag[index].equals(anEntry)) {
+            found = true;  // 设置找到标志为true
+        } else {
+            index++;  // 否则，索引加1，检查下一个元素
+        }
+    }
+    return found;  // 返回是否找到目标元素的布尔值
+}
+
+    // 返回数组中的所有元素的字符串表示形式
+public String toString() {
+    String strResult = "Bag[ \n";  // 初始化结果字符串，并添加开头的"Bag["
+    // 遍历数组
+    for (int i = 0; i < this.numberOfEntries; i++) {
+        strResult += this.bag[i] + "\n";  // 将每个元素添加到结果字符串中，并在每个元素后添加换行符
+    }
+    strResult += "]";  // 在结果字符串的末尾添加"]"
+    return strResult;  // 返回结果字符串
 }
 }
